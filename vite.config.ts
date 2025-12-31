a38611e096c14f18e8ff5f19fb4e703bd3a68a9d
豌豆 <punkpea@Mac-mini.local>
2026-01-01 01:20:24 +0800

    remove gemini and fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// 因为您的仓库名字是 exe，所以这里要写成 /exe/
 export default defineConfig({
   plugins: [react()],
-  base: '/exe/', 
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY || '')
-  },
+  base: '/exe/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 });

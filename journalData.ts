6efc5a12fdb249a14a9381461f133cef6ee84e65
豌豆 <punkpea@Mac-mini.local>
2026-01-01 07:28:41 +0800

    journal

--- journalData.ts
+++ journalData.ts
@@ -5,29 +5,30 @@ const currentMonth = today.getMonth() + 1;
 const currentDay = today.getDate();
 
 export const MOCK_JOURNAL: JournalEntry[] = [
-  {
-    id: 'j-2023',
-    year: 2023,
-    month: currentMonth,
-    day: currentDay,
-    content: '开始尝试记录生活。发现大部分时间其实都在处理琐事，只有深夜是属于自己的。',
-    mood: 'Start'
-  },
-  {
-    id: 'j-2024',
-    year: 2024,
-    month: currentMonth,
-    day: currentDay,
-    content: '又到了这一天。回看去年的记录，心态已经完全不同了。这就是所谓的螺旋上升吗？',
-    mood: 'Loop'
-  },
+
   // 你可以手动在这里添加更多“历史数据”，它们会根据日期自动显示
   {
-    id: 'j-demo-1',
-    year: 2022,
+    id: 'j-2025-01-01',
+    year: 2025,
     month: 1,
     day: 1,
     content: '新年快乐。',
     mood: 'New'
-  }
+  },
+   {
+    id: 'j-2025-07-30',
+    year: 2025,
+    month: 7,
+    day: 30,
+    content: '29 岁生日快乐。',
+    mood: 'Birthday'
+  },
+  {
+    id: 'j-2026-01-01',       // 唯一ID，随便写，别重复就行
+    year: 2022,               // 年份
+    month: 1,                // 月份
+    day: 1,                  // 日期
+    content: 'Hi,2026。', // 日记内容
+    mood: 'Melancholy'        // (可选) 当时的心情标签
+  },
 ];
